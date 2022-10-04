fafbfbac2186deb5e05f3cf9b3e76c104f66e13f
anishkam98 <94488908+anishkam98@users.noreply.github.com>
2022-10-03 21:46:54 -0400

    Delete Sample3 directory

--- Sample3/flask/classes.py
+++
@@ -1,6 +0,0 @@
-class Users:
-    def __init__(self, userid, username, firstname, lastname):
-        self.userid = userid
-        self.username = username
-        self.firstname = firstname
-        self.lastname = lastname
